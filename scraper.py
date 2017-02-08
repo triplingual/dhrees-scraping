35142f9e85cf0662c111e78dd7ac5562e9d42263
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2017-02-08 13:35:06 -0500

    Edits as of 8 Feb 2017

--- scraper.py
+++ scraper.py
@@ -27,8 +27,19 @@ poem_directory = "feb-web"
 if not os.path.exists(poem_directory):
     os.makedirs(poem_directory)
 
+
+# loop from 001 to 384
+for page in range(1, 33):
+  try:
+    page_number = page.zfill
+    response = urllib2.urlopen("http://feb-web.ru/feb/mayakovsky/texts/ms0/ms1/ms1-034-.htm?cmd=2")
+  except urllib2.HTTPError, e:
+    continue
+
+# try
+# catch HTTPError 404 page
+
 # request the landing page for the site
-#response = urllib2.urlopen("http://feb-web.ru/feb/mayakovsky/texts/ms0/ms1/ms1-033-.htm?cmd=2")
 response = urllib2.urlopen("http://feb-web.ru/feb/mayakovsky/texts/ms0/ms1/ms1-034-.htm?cmd=2")
 
 # get the html content using windows 1251 encoding
@@ -45,23 +56,16 @@ poem_title = poem.contents[0].contents[0].string
 poem_stanzas = soup.find_all("p", class_=re.compile("stih10ot"))
 
 poem_stanzas[0].find("div", style="display:none;").extract()
-poem_date = poem_stanzas[0].find("p", class_="podp-lev").extract()
-
-"""
-print(poem_title)
-print(poem_stanzas[0])
-print(poem_date)
-"""
+poem_date = poem_stanzas[0].find("p", class_="podp-lev").extract().i.string
 
 if len(poem_stanzas[0]) > 0:
-  '''
-  title = title.replace("/", "-")
-  title = title.replace(":", "-").strip().strip(",\".")
-  '''
 
-  body = str(poem_stanzas[0])
+# TODO: extract page number SPANs
+# TODO: extract line number SPANs
+# NOTE: poem named "150 000 000" has formatting that might hork things
+  body = poem_stanzas[0].getText()
   author = "mayakovsky"
-  date = "foo"
+  date = poem_date
 
   fp = codecs.open(poem_directory + "/" + author + "--" + poem_title + "--" + date + ".txt", "w", "utf-8")
   fp.write(body)
@@ -72,91 +76,4 @@ if len(poem_stanzas[0]) > 0:
 
 
 """
-
-# examine each a tag object in turn
-for a_tag in a_tags[:3]:
-
-  # convert beautiful soup object to a unicode (string) object
-  a_tag = unicode(a_tag)
-
-  # split the a tag string each time we see href="
-  split_a_tag = a_tag.split('href="')
-
-  # grab the 2nd member of the list (0-based indexing)
-  split_link = split_a_tag[1]
-
-  # split that object on ">
-  link_list = split_link.split('">')
-
-  # get the clean link value
-  link = link_list[0]
-
-  # fetch the html content for link
-  response = urllib2.urlopen(link)
-
-  ###
-  # Poet page
-  ###
-
-  # read the html content from the connection
-  html = response.read()
-
-  # decode the html content
-  decoded = unicode(html, "windows-1251")
-
-  # transform the html into a soup object
-  soup = BeautifulSoup(decoded, 'html.parser')
-
-  # find all author tags
-  a_tags = soup.find_all("a", class_="mainlevel")
-
-  # examine each member of the a_tags list
-  for a_tag in a_tags:
-
-    # get the string content of a_tag
-    a_tag = unicode(a_tag)
-
-    # split the string each time you see "
-    quote_list = a_tag.split('"')
-
-    # get the forth member of the quote list
-    link = quote_list[3]
-
-    ###
-    # Poem page
-    ###
-
-    # fetch the html for the poem page
-    response = urllib2.urlopen(link)
-
-    # read the html content from the connection
-    html = response.read()
-
-    # decode the html content
-    decoded = unicode(html, "windows-1251")
-
-    # transform the html into a soup object
-    soup = BeautifulSoup(decoded, 'html.parser')
-
-    # find all table tags with class = contentpaneopen on the poem page
-    poem_content = soup.find_all("table", class_="contentpaneopen")
-
-    if len(poem_content) > 0:
-      title = poem_content[0].find("td").string
-      title = title.replace("/", "-")
-      title = title.replace(":", "-").strip().strip(",\".")
-
-      body_author = poem_content[1].find("td")
-      body = body_author.get_text()
-      author = body_author.find("div").string
-
-#    print link, title, body
-
-      fp = codecs.open(poem_directory + "/" + author + "--" + title + ".txt", "w", "utf-8")
-      fp.write(body)
-      fp.close()
-
-    # write the poem to the filesystem
-    with codecs.open(poem, "w", "utf-8") as out:
-      out.write(poem)
-    """
+"""
