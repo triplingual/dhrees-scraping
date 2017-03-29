57a3a343820eb8144c211d6b68456335b9c20a1d
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2017-03-29 12:08:13 -0400

    Fleshed out a little.

--- blok-extract-page-numbers.py
+++ blok-extract-page-numbers.py
@@ -11,10 +11,27 @@ Iterate over results
 Close files
 '''
 # Open file for reading
-fp = open("/Users/trip/Coding/TextMining/Bozovic-Rosenkranz-S15/dhrees-scraping/blok_sobranie_sochineny_v_8-mi_tt_tom1_1960__ocr-index.txt")
+fp = open("/Users/trip/Coding/TextMining/Bozovic-Rosenkranz-S15/"
+          "dhrees-scraping/blok_sobranie_sochineny_v_8-mi_tt_tom1_1960__ocr-index.txt")
 # Create regex pattern to find all numbers
 patt = re.compile("\d*")
 # Execute regex into variable
-numbers = patt.match(fp.read)
+numbers = patt.findall(fp.read())
+# close original file
+fp.close()
+
+if numbers:
+    print 'Matches found: ', len(numbers)
+else:
+    print 'No matches'
+
+'''
+NEXT:
+Use scraped numbers tuple to read through main file line by line, writing each line to output file
+closing output file if page number of page break line matches number in the numbers tuple
+'''
+
 # Open file for writing
 fp = open("/Users/trip/Coding/TextMining/Bozovic-Rosenkranz-S15/dhrees-scraping/number-list.txt", "w")
+fp.write(numbers)
+fp.close()
